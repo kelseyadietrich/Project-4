cea53fcde2c0b23433d76f2a99cab82c2bdd8a7d
kelseyadietrich <dietrichkk@hendrix.edu>
2017-04-18 18:04:15 -0500

    Controller changes

---
+++ src/controllers/GuiMain.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class GuiMain extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(GuiMain.class.getResource("ChooseLayout.fxml"));
+		AnchorPane root = (AnchorPane) loader.load();
+
+		Scene scene = new Scene(root, 507, 300);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args){
+		launch(args);
+	}
+}
