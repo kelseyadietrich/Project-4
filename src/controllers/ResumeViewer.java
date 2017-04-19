2c8684c780b908d8d8ba1278d9aa6d73f3dd01ba
Bosco Ndemeye <ndemeyemm@hendrix.edu>
2017-04-19 18:52:31 -0500

    Create ResumeViewer, add testHello.html

---
+++ src/controllers/ResumeViewer.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class ResumeViewer {
+	private WebView browser;
+	private WebEngine webEngine;
+	
+	
+	public ResumeViewer(){
+		this.browser = new WebView();
+		this.webEngine = browser.getEngine();
+	}
+	
+	
+	
+	
+	public void DisplayContentsOf(String filelocation){
+		Stage webStage = new Stage();
+		webStage.setWidth(400);
+		webStage.setHeight(500);
+		
+		Scene scene = new Scene(new Group());
+		ScrollPane scrollPane = new ScrollPane();
+	    scrollPane.setContent(this.browser);
+	    
+	    webEngine.load(filelocation);
+
+	    scene.setRoot(scrollPane);
+
+	    webStage.setScene(scene);
+	    webStage.show();
+	} 
+
+}
