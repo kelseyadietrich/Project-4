ab06fced5426f5b8092d346f68158725bf68864d
Bosco Ndemeye <ndemeyemm@hendrix.edu>
2017-04-21 09:27:40 -0500

    Fix open HTML.html nullPointer

--- src/resumeViewer/LaunchResumeExampleController.java
+++ src/resumeViewer/LaunchResumeExampleController.java
@@ -15,7 +15,7 @@ public class LaunchResumeExampleController {
 	void initialize(){
 		ResumeViewer myViewer = new ResumeViewer();
 		launch.setOnAction(event -> {
-			myViewer.DisplayContentsOf("src/HTML.html");
+			myViewer.DisplayContentsOf("../HTML.html");
 		});
 		
 	}
